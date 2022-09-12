1b58717b43f43caa9fbbb59840a9b2e096e2af00
GGMaster <rilckgg@gmail.com>
2022-09-12 13:13:08 -0300

    added yield method

--- resources/LeagueReader.py
+++ resources/LeagueReader.py
@@ -3,8 +3,7 @@ from models import PlayerEntity
 from models.Minion import MinionEntity
 from resources import offsets, LeagueStorage
 from resources.StructureReader import StructureReader
-
-
+from functools import cached_property
 class LeagueReader:
     def __init__(self, pm: Pymem, mem, overlay, viewProjMatrix, lStorage):
         self.pm = pm
@@ -12,37 +11,33 @@ class LeagueReader:
         self.overlay = overlay
         self.viewProjMatrix = viewProjMatrix
         self.lStorage: LeagueStorage = lStorage
-
-        self.localPlayer: PlayerEntity = self.get_local_player()
-        self.teamPlayers, self.enemyPlayers = self.get_players()
-        self.minions = self.get_minions()               
-
-    def get_minions(self) -> list:   
+        
+    @cached_property
+    def minions(self) -> list:   
         all_minions_addr: list[int] = StructureReader.read_v_table(self.pm, self.lStorage.minion_manager_addr)        
         for minion_addr in all_minions_addr:            
             m = MinionEntity(self.pm, self.mem, self.overlay, self.viewProjMatrix, minion_addr)
             if m.health > 0:
                 yield m               
-
+    @cached_property
     def get_local_player(self) -> PlayerEntity:
         return PlayerEntity(self.pm, self.mem, self.overlay, self.viewProjMatrix, self.lStorage.localPlayerAddr)
-
-    def get_players(self) -> tuple[list[PlayerEntity], list[PlayerEntity]]:
+    
+    @cached_property
+    def team_players(self) -> list[PlayerEntity]:
+        for p in self.get_players():
+            if p.teamId == self.localPlayer.teamId:
+                yield p
+    @cached_property
+    def enemies_players(self) -> list[PlayerEntity]:
+        for p in self.get_players():
+            p.teamId != self.localPlayer.teamId
+            yield p
+    
+    @cached_property
+    def get_players(self) -> tuple[list[PlayerEntity], list[PlayerEntity]]:       
+        
         allChampAddrs: list[int] = StructureReader.read_v_table(self.pm, self.lStorage.heroManagerAddr)
 
-        teamPlayers, enemyPlayers = [], []
-        i: int = 0
-        while i < len(allChampAddrs):
-            isVisible: bool = self.pm.read_bool(allChampAddrs[i] + offsets.oObjVisible)
-            health: float = self.pm.read_float(allChampAddrs[i] + offsets.oObjHealth)
-
-            if isVisible or health <= 0:
-                player: PlayerEntity = PlayerEntity(self.pm, self.mem, self.overlay, self.viewProjMatrix,
-                                                    allChampAddrs[i])
-                if player.teamId == self.localPlayer.teamId:
-                    teamPlayers.append(player)
-                else:
-                    enemyPlayers.append(player)
-            i += 1
-
-        return teamPlayers, enemyPlayers
+        for champ in allChampAddrs:
+            yield champ
