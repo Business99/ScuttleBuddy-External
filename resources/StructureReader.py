579819bbf099c8b8167e24f22c463e315edc8f44
GGMaster <rilckgg@gmail.com>
2022-09-12 12:35:14 -0300

    added yield method

--- resources/StructureReader.py
+++ resources/StructureReader.py
@@ -5,12 +5,8 @@ class StructureReader:
 
     @staticmethod
     def read_v_table(pm: Pymem, addr: int) -> list[int]:
-        result: list[int] = []
-
         lst: int = pm.read_int(addr + 0x4)
         size: int = pm.read_int(addr + 0x8)
-
+        
         for i in range(0, size, 1):
-            result.append(pm.read_int(lst + (i * 0x4)))
-
-        return result
+            yield pm.read_int(lst + (i * 0x4))
