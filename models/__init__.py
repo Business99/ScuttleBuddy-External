dd8bbb2e9e855be401a32a4bb4e36f6f369b2e49
GGMaster <rilckgg@gmail.com>
2022-09-12 18:35:12 -0300

    fixed import

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,4 @@ from models.Entity import Entity
 from models.Spell import Spell
 from models.PlayerEntity import PlayerEntity
 from models.GameState import GameState
-from models.EntityManager import EntityManager
+from models.EntityManager import Model
